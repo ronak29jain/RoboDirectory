73f611aa2bbb3ea80cd1bf83524f5c673c237f92
Ronak Jain <ronak29jain@gmail.com>
2022-07-17 02:10:15 +0530

    Added Scroll and Way to use Online user data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import Searchbox from "./Searchbox";
 import CardList from "./CardList";
+import Scroll from "./Scroll";
 import { directory } from "./directory";
-
+import './App.css';
 
 class App extends Component {
     constructor() { 
@@ -11,7 +12,15 @@ class App extends Component {
             directory: directory,
             searchfield: "",
         }
-    }
+    } 
+
+    // also set the directory in state as empty if you use this as directory will update when user list is fetched from the server (directory: [])
+
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({directory: users}))
+    // }
 
     whenISearch = (event) => {
         this.setState({searchfield: event.target.value})
@@ -21,31 +30,22 @@ class App extends Component {
         const filteredDirectory = this.state.directory.filter(directory => {
             return directory.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1>RoboDirectory</h1>
-                <Searchbox searchChange = { this.whenISearch }/>
-                <CardList directory = { filteredDirectory }/>
-            </div>
-        )
+
+        if(this.state.directory.length === 0){
+            <h1>loading</h1>
+        }
+        else {
+            return (
+                <div className="tc">
+                    <h1>RoboDirectory</h1>
+                    <Searchbox searchChange = { this.whenISearch }/>
+                    <Scroll>
+                        <CardList directory = { filteredDirectory }/>
+                    </Scroll>
+                </div>
+            )
+        }
     }
 }
-
-
-
-
-
-
-
-// const App = () => {
-//     return (
-//         <div className="tc">
-//             <h1>RoboDirectory</h1>
-//             <Searchbox />
-//             <CardList directory={ directory }/>
-//         </div>
-//     )
-// }
-
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import './Card.css';
 
 const Card = ({ id, name, phone, email }) => {
     // const { id, name, phone, email } = props;
     return (
-        <div className="tc bg-light-green dib br3 ma2 pa3 grow bw2 shadow-5">
+        <div id="card" className="tc dib br3 ma2 pa3 grow bw2 shadow-5">
             <img alt='robot_image' src= {`https://robohash.org/${email}?200*200`} />
             <div>
                 <h2>{name}</h2>
